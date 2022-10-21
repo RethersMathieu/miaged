2422fb99e606fea1b7dbd49d3feb8631ddd8aa4b
Mathieu Rethers <71334121+RethersMathieu@users.noreply.github.com>
2022-10-21 14:51:30 +0200

    Router

--- lib/router.dart
+++ lib/router.dart
@@ -1,25 +1,43 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:miaged/firebase/auth.dart';
 import 'package:miaged/pages/home.dart';
 import 'package:miaged/pages/log.dart';
+import 'package:miaged/scaffold_bottom_navbar.dart';
 
 class RouteConfig {
+  static final _rootNavigatorKey = GlobalKey<NavigatorState>();
+  static final _shellNavigatorKey = GlobalKey<NavigatorState>();
+
   static GoRouter instance = GoRouter(routes: <GoRoute>[
     GoRoute(
         path: '/',
-        builder: (context, state) => Home(),
-        redirect: (context, state) async {
-          User? user = FirebaseAuth.instance.currentUser;
-          return user == null ? '/login' : null;
-        }
+        builder: (context, state) => const Home(),
+        redirect: (context, state) async => !Auth.profilUser.isLogged ? '/login' : null,
+        routes: [
+          ShellRoute(
+            navigatorKey: _shellNavigatorKey,
+            builder: (context, state, child) => ScaffoldWithBottomNavBar(
+                items: const [
+                  ScaffoldWithNavBarTabItem(initialLocation: '', icon: Icon(Icons.store), label: 'Vitrine'),
+                  ScaffoldWithNavBarTabItem(initialLocation: 'shapping_cart', icon: Icon(Icons.shopping_basket), label: 'Panier'),
+                  ScaffoldWithNavBarTabItem(initialLocation: 'profil', icon: Icon(Icons.person), label: 'Profile')
+                ],
+                child: child
+            ),
+            routes: [
+              GoRoute(
+                path: '',
+                pageBuilder: (context, state) => NoTransitionPage(child: null)
+              ),
+            ]
+          )
+        ]
     ),
     GoRoute(
         path: '/login',
         builder: (context, state) => LogIn(),
-        redirect: (context, state) async {
-          User? user = FirebaseAuth.instance.currentUser;
-          return user != null ? '/' : null;
-        }
+        redirect: (context, state) async => Auth.profilUser.isLogged ? '/' : null,
     )
   ]);
 }
